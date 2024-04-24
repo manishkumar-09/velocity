58a3f65fb74c31acf536a12ccc7f9fb7104ae40d
Manish <iammanish0312@gmail.com>
2024-04-24 13:18:34 +0530

    fix the balance transfer logic

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -21,43 +21,53 @@ module.exports = {
   balanceTransfer: async (req, res) => {
     //creating session using mongoose
     const session = await mongoose.startSession();
-
     session.startTransaction();
-    const { amount, to } = req.body;
 
-    //find the user associated with account and .session(session) method ensures that the query is executed within the context of the ongoing MongoDB session
-    const account = await Account.findOne({ userId: req.user }).session(
-      session
-    );
-    console.log(account);
-    if (!account || account.balance < amount) {
-      await session.abortTransaction();
-      return res.status(400).json({
-        message: "Insufficient balance",
+    try {
+      const { amount, to } = req.body;
+
+      //find the user associated with account and .session(session) method ensures that the query is executed within the context of the ongoing MongoDB session
+      const senderAccount = await Account.findOne({ userId: req.user }).session(
+        session
+      );
+
+      if (!senderAccount || senderAccount.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+          message: "Insufficient balance",
+        });
+      }
+
+      const toAccount = await Account.findOne({ userId: to }).session(session);
+
+      if (!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+          message: "Invalid account",
+        });
+      }
+
+      // transfer the amount to the receiver account
+
+      await Account.updateOne(
+        { userId: req.user },
+        { $inc: { balance: -amount } }
+      ).session(session);
+      await Account.updateOne({ userId: to }, { $inc: { balance: amount } });
+
+      // commit the transaction
+      await session.commitTransaction();
+      session.endSession();
+      res.status(200).json({
+        message: "Transfer Successfull",
       });
-    }
-
-    const toAccount = await Account.findOne({ userId: to }).session(session);
-    if (!toAccount) {
+    } catch (err) {
       await session.abortTransaction();
-      return res.status(400).json({
-        message: "Invalid account",
-      });
+      session.endSession();
+
+      console.error("Transaction aborted:", err);
+      res.status(500).json({ message: "Internal server error" });
     }
-
-    // transfer the amount to the receiver account
-
-    await Account.updateOne(
-      { userId: req.userId },
-      { $inc: { balance: -amount } }
-    ).session(session);
-    await Account.updateOne({ userId: to }, { $inc: { balance: amount } });
-
-    // commit the transaction
-    await session.commitTransaction();
-    res.status(200).json({
-      message: "Transfer Successfull",
-    });
   },
   //
 };
