d4b8bfd0a9978648b3e8bd411a6765693f997977
Manish <iammanish0312@gmail.com>
2024-04-24 14:03:45 +0530

    updated schema

--- backend/models/accountModel.js
+++ backend/models/accountModel.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 const accountSchema = new Schema(
   {
-    user_id: {
+    userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: true,
